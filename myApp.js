93d1193dff6be7f606a52921dd9479dc21ba7d8a
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-01-22 21:40:10 +0100

    added / and /json endpoints

--- myApp.js
+++ myApp.js
@@ -1,7 +1,35 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
-console.log("Hello World!")
+htmlPath = __dirname + '/views/index.html';
+assetsPath = __dirname + '/public';
+
+
+app.use('/public', express.static(assetsPath));
+
+app.get(
+    '/',
+    function(req, res) {
+        res.sendFile(htmlPath);
+    }
+);
+
+app.get(
+    '/json',
+    function(req, res) {
+        if(process.env.MESSAGE_STYLE === "uppercase"){
+            res.json({"message": "HELLO JSON"});
+        }
+        else{
+            res.json({"message": "Hello json"});
+        }
+    }
+);
+
+
+
+
 
 
 
